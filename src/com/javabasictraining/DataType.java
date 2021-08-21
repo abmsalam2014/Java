fe7960b8ece6cec5dc8923b9ab1b70331fcd8cb3
mehediaziz <mehediaziz@yahoo.com>
2021-08-21 10:25:28 -0400

    hello world & data type

---
+++ src/com/javabasictraining/DataType.java
@@ -0,0 +1,35 @@
+package com.javabasictraining;
+
+public class DataType {
+
+    public static void main(String[] args) {
+
+
+        // Java 2 types data type:
+        //1. Primitive DataType
+        //2. Reference DataType(Object)
+
+        //1. PrimitveDataType
+            //1. byte
+            //2. short
+            //3. int *
+            //4. long
+            //5. double *
+            //6. float
+            //7. char
+            //8. boolean *
+
+        // //2. Reference DataType(Object)
+            //String *
+
+        byte abc = 10;          // -128 to +127
+        short bcd = 11;         // -32768 to +32767
+        int cde = 12;           // -2147483648 to +2147483647 *
+        long dcf = 13l;          // -9223372036854775808 to +9223372036854775807
+        double fgh = 13.12345678912365456789;// .15 *
+        float ghk = 13.12345678912f;  //.6
+        char hki = '$';
+        boolean hkj = true; //true or false
+        String kji = "java training 2021";  // " " ***
+    }
+}
