af298ff8019b57cdf4c887643c83db67b549eec8
mehediaziz <mehediaziz@yahoo.com>
2021-08-22 10:37:31 -0400

    added new class concatenation

---
+++ src/com/javabasictraining/Concatenation.java
@@ -0,0 +1,18 @@
+package com.javabasictraining;
+
+public class Concatenation {
+
+    public static void main(String[] args) {
+
+        String firstName = "Mehedi";
+        String lastName = "Aziz";
+        int age = 35;
+        int ssn = 12345678;
+
+        String fullName = firstName + " " + lastName + "," + age + "," + ssn;
+        System.out.println(fullName); //printlinenumber
+        //System.out.print(fullName);  //print
+        //System.out.println(fullName);
+
+    }
+}
