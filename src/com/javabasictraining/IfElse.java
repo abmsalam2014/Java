1cc7f018e0f42626aae26e88610adca73aeaea04
mehediaziz <mehediaziz@yahoo.com>
2021-08-28 10:54:05 -0400

    if else class

---
+++ src/com/javabasictraining/IfElse.java
@@ -0,0 +1,24 @@
+package com.javabasictraining;
+
+public class IfElse {
+
+
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b = 5;
+
+        if (a < b) {
+            System.out.println("Hello");
+        } else if (a < b) {
+            System.out.println("Hello2");
+
+        } else if (a==b){
+            System.out.println("Hello3");
+        } else {
+            System.out.println("Hello4");
+        }
+    }
+
+}
